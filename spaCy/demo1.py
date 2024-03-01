d0983341cb4f757e8700aa42fcd1f37be7800864
u03013112 <u03013112@hotmail.com>
2024-03-01 10:19:30 +0800

    123

--- spaCy/demo1.py
+++ spaCy/demo1.py
@@ -14,10 +14,10 @@ doc = nlp(text)
 # 提取句子
 sentences = [sent for sent in doc.sents]
 
-for i, sent in enumerate(sentences):
-    print(f">>>> {i}: {sent.text}")
+# for i, sent in enumerate(sentences):
+#     print(f">>>> {i}: {sent.text}")
 
-print("------------------")
+# print("------------------")
 
 # 句子打断，有些说话旁白和说话内容在同一句话中，需要打断成旁白和说话内容两句话
 def break_sentences(sentences, speech_verbs):
@@ -32,9 +32,16 @@ def break_sentences(sentences, speech_verbs):
             broken_sentences.append(sent)
             continue
 
-        # 查找说话动词后面的标点符号位置
+        
         for i, token in enumerate(tokens):
             if token.text in speech_verbs:
+                # 查找说话动词前面的”号位置
+                for j in range(i - 1, -1, -1):
+                    if tokens[j].text == "”":
+                        break_points.append(j)
+                        break
+
+                # 查找说话动词后面的“号位置
                 for j in range(i + 1, len(tokens)):
                     if tokens[j].text == "“":
                         break_points.append(j-1) 
@@ -42,6 +49,7 @@ def break_sentences(sentences, speech_verbs):
 
         # 如果找到了打断点，将句子拆分为多个部分
         if break_points:
+            # print(f"sentence: {sent.text}, break_points: {break_points}")
             start = 0
             for point in break_points:
                 broken_text = (sent[start:point+1].text)
@@ -59,11 +67,11 @@ def break_sentences(sentences, speech_verbs):
     return broken_sentences
 
 # 创建词汇表
-speechVerbs = ["说", "道", "告诉", "问", "回答"]
+speechVerbs = ["说", "道", "便道", "告诉", "问", "回答"]
 
 sentencesBroken = break_sentences(sentences, speechVerbs)
-for i, sent in enumerate(sentencesBroken):
-    print(f">>>> {i}: {sent}")
+# for i, sent in enumerate(sentencesBroken):
+#     print(f">>>> {i}: {sent}")
 
 
 # 句子合并，有些说话内容是多句话，所以将多句话合并成一句话，用引号来判断
@@ -90,30 +98,32 @@ def merge_sentences(sentences, max_merge_count=3):
 
             # 如果在范围内找到”，则进行合并
             if "”" in sentences[temp_i].text:
-                sentences_merged.append(merged_text)
+                doc = nlp(merged_text)
+                span = doc[:]
+                sentences_merged.append(span)
                 i = temp_i
             else:
                 # 如果在范围内没有找到”，则取消合并
                 for j in range(i, temp_i + 1):
-                    sentences_merged.append(sentences[j].text)
+                    sentences_merged.append(sentences[j])
                 i = temp_i
         else:
-            sentences_merged.append(sent.text)
+            sentences_merged.append(sent)
 
         i += 1
     
     return sentences_merged
 
-# sentencesMerged = merge_sentences(sentencesBroken, 5)
 
-# # 输出合并后的句子
-# for i, sent in enumerate(sentencesMerged):
-#     print(f">>>> {i}: {sent}")
+
+sentencesMerged = merge_sentences(sentencesBroken, 5)
+
+# 输出合并后的句子
+for i, sent in enumerate(sentencesMerged):
+    print(f">>>> {i}: {sent}")
 
 
 # TODO：
-
-
 # 说话内容找主人，找不到主人的就先当做旁白处理
 
 
